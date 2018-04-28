77ac2450af1da0905bf0550844f66b0ca3a59b2f
DariaReva <38789189+DariaReva@users.noreply.github.com>
2018-04-28 14:06:15 +0300

    Add files via upload

---
+++ seleniumproject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace seleniumproject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestCleanup]
+        public void Clean()
+        {
+
+        }
+    }
+}
